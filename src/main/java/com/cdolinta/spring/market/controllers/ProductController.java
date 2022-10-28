a547683a87412773b9eaab1e5d0f64a91526da18
CristianSw <cristian.sw@protonmail.com>
2022-10-28 14:19:40 +0000

    Initial project partially done, stopped on token Util

---
+++ src/main/java/com/cdolinta/spring/market/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package com.cdolinta.spring.market.controllers;
+
+import com.cdolinta.spring.market.entities.Product;
+import com.cdolinta.spring.market.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> findAllProducts() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProductById(@PathVariable Long id) {
+        return productService.findById(id).get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProductById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+}
