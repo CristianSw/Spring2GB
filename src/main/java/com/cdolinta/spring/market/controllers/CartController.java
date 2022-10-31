6aa13aaf0b15c607a7079210e4a260ab91dfdd62
CristianSw <cristian.sw@protonmail.com>
2022-10-31 12:03:28 +0000

    Homework 1 done.

---
+++ src/main/java/com/cdolinta/spring/market/controllers/CartController.java
@@ -0,0 +1,28 @@
+package com.cdolinta.spring.market.controllers;
+
+import com.cdolinta.spring.market.entities.Product;
+import com.cdolinta.spring.market.services.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartController {
+    private final CartService cartService;
+
+    @GetMapping
+    public List<Product> findAllProducts(){
+        return cartService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public void addProductToCart(@PathVariable long id){
+        cartService.addProduct(id);
+    }
+}
